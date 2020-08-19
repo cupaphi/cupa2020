bd95c3922d2efb9b70be90e4dbab8c256537b243
Vlad Seremet <vladseremet@gmail.com>
2020-08-19 19:43:07 +0300

    Updates

---
+++ precache-manifest.8e3a6350bb8f1a0682a931be2aa49bc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb6b4b2c0d63d76f6a1a8850b7d5ac5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88be1eb73c6f3131c60d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb71e1c41282f8063f21",
+    "url": "/static/js/2.cbf49d20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cbf49d20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88be1eb73c6f3131c60d",
+    "url": "/static/js/main.a6a3a7fe.chunk.js"
+  },
+  {
+    "revision": "e8830a6bb2cfaec47850",
+    "url": "/static/js/runtime-main.790e347d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
