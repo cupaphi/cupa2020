86db72d4f089710f8fde1392206a371bb3b7d5c9
Vlad Seremet <vladseremet@gmail.com>
2020-08-19 19:56:41 +0300

    Updates

---
+++ precache-manifest.3e114caf1b54a20333402d8264f91a7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95788a2482bcd6af0b7ba9006b1f8b5a",
+    "url": "/cupa2020/index.html"
+  },
+  {
+    "revision": "ec2f31b63df0d852c245",
+    "url": "/cupa2020/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb71e1c41282f8063f21",
+    "url": "/cupa2020/static/js/2.cbf49d20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cupa2020/static/js/2.cbf49d20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec2f31b63df0d852c245",
+    "url": "/cupa2020/static/js/main.0973f12f.chunk.js"
+  },
+  {
+    "revision": "1efe91f6b667f7fdbc3e",
+    "url": "/cupa2020/static/js/runtime-main.f5fcba14.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cupa2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
